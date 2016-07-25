9cef60fce9075b4154215a282d24afd3ed5de4cc
Bo Brinkman <dr.bo.brinkman@gmail.com>
2016-07-25 11:45:00 -0400

    Post ip address where I can easily look it up

--- main.cpp
+++ main.cpp
@@ -30,7 +30,60 @@
 #include "constants.h"
 #include "tracker.h"
 
+//Next 5 includes are for the exec() method
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+
+#include <boost/network/protocol/http/client.hpp>
+
+std::string exec(const char* cmd) {
+  char buffer[128];
+  std::string result = "";
+  std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) throw std::runtime_error("popen() failed!");
+  while (!feof(pipe.get())) {
+    if (fgets(buffer, 128, pipe.get()) != NULL)
+      result += buffer;
+  }
+  return result;
+}
+
+void updateIPDiscoveryServer(){
+  //TODO: Using exec to call ifconfig is probably not the right way to
+  // to do this
+  std::string res = exec("/sbin/ifconfig");
+  std::istringstream ifconfigin(res);
+  std::string nibble;
+  while(ifconfigin >> nibble, nibble != "wlan0"){
+    //Do nothing, just throw it all away
+  }
+  while(ifconfigin >> nibble, nibble != "inet"){
+        //Do nothing, just throw it all away
+  }
+  ifconfigin >> nibble;
+  std::string address(nibble.begin() + 1 + nibble.find(":"),
+		      nibble.end());
+  std::cout << "Server running at: |" << address << "|" << std::endl;
+  http::client client_;
+  try {
+    http::client::request request_("http://shelvar.com/ip.php?ip=" + address);
+    //request_ << http::client::header("Conection", "close");
+    
+    http::client::response response_ = client_.get(request_);
+    std::cout << "Discovery service update result: " << body(response_)
+	      << std::endl;;
+  } catch (std::exception& e) {
+    // deal with exceptions here
+  }
+}
+
 int main() {
+  updateIPDiscoveryServer();
+  
   //Build the LUT before opening the mic
   LocationLUT& lut = LocationLUT::getInstance();
   Tracker& t = Tracker::getInstance();
